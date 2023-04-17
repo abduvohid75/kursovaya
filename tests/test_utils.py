bd26fa8bfb243c91b179e1598e8a812935bda3ea
abduvohid75 <128813776+abduvohid75@users.noreply.github.com>
2023-04-17 16:56:28 +0500

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from utils import sort_data
+from utils import sort_data, filter_data, format_data
 
 
 def test_sort_data(test_data):
@@ -7,7 +7,11 @@ def test_sort_data(test_data):
     sorted_data = sort_data(test_data)
     assert [x['date'] for x in sorted_data] == ['2019-08-26T10:50:58.294041', '2019-07-03T18:35:29.512364']
 
+def test_filter_data(test_data):
+    dated = filter_data(test_data)
+    assert [x['state'] for x in dated] == ['EXECUTED', 'EXECUTED']
 
-
-
-
+def test_format_data(test_data):
+    formated = format_data(test_data)
+    assert  [x[1:11] for x in formated] == ['26.08.2019', '03.07.2019']
+    assert [x[32:39] for x in formated] == ['Maestro', 'MasterC']
